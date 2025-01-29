1a80086dfabb6ddc8349e747a4378c44aa3b56fc
nachonavazo13 <nachonavazo13@hotmail.com>
2025-01-29 16:19:00 -0300

    Pimera app

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
 # python -m venv ev_streamlit25 como crear un entorno virtual
 #ev_streamlit25\Scripts\activate asi activo el entorno virtual
+# streamlit run app.py    asi ejecuto streamlit
+# Con Ctrl + C cortamos la ejecucion
+
+
+# importar las librerias
+
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# configuracion de la app
+def main():
+    st.title('App con Streamlit 25')
+    st.subheader('Esta app permite visualizar datos de la NBA')
+
+
+# Crear ejecutable
+if __name__ == "__main__":
+    main()
+
+
